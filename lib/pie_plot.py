739e9696a87ddc88fc3bccc862c5fac080d7686f
kaxp <kapilsahu232@gmail.com>
2025-11-09 17:02:09 +0530

    Add pie plot

---
+++ lib/pie_plot.py
@@ -0,0 +1,14 @@
+import numpy as np
+import matplotlib.pyplot as plt 
+
+languages = ["Python", "C++", "C#", "Java", "Go"]
+votes = [45, 19, 8, 28, 35]
+
+explodes = [0, 0, 0.2, 0, 0]
+
+# explodes is to highlight a particular cell
+# autopct is to write the percentage in chart
+# pctdistance is distance from center
+# startangle is to define angle where to start the chart 
+plt.pie(votes, labels=languages, explode=explodes, autopct="%.2f%%", pctdistance=0.8, startangle=90)
+plt.show()

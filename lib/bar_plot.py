5e18b335e52e287993ed53dd40b5cb5ce7fb2fe3
kaxp <kapilsahu232@gmail.com>
2025-11-09 17:02:58 +0530

    Add bar plot

---
+++ lib/bar_plot.py
@@ -0,0 +1,12 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+x = ["C++", "C#", "Python", "Java", "Go"]
+
+y = [42, 35, 69, 23, 51]
+
+plt.bar(x, y, color='r', align='edge', width=0.4)
+plt.show()
+
+

32379f3fb8209876359888145a59217b5d471fa1
kaxp <kapilsahu232@gmail.com>
2025-11-12 19:12:39 +0530

    Update histogram plot

--- lib/histogram.py
+++ lib/histogram.py
@@ -1,17 +1,37 @@
-import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
 
-# normal distribution(mean, standard deviation, count)
-ages = np.random.normal(20, 1.5, 1000)
+# Histogram are a bit similar to Bar graph, the difference here is that
+# we plot histogram in bins. So let's say we have hundreds of data that we
+# need to plot and if using Bar or other graph its not good as the data will
+# clatter. So in those conditions we use histogram where we define bins.
+# For e.g if we define bins = 5, then it means over entire data in graph will
+# be divided and plotted in 5 sections(bins)
 
-# plt.hist(ages)
+plt.style.use("fivethirtyeight")
 
-# bins is number of bars(bins)
-# min age to 18, 18 to 21 and 21 to max age
-# plt.hist(ages, bins=[ages.min(), 18, 21, ages.max()])
+data = pd.read_csv("lib/data/ages.csv")
+ids = data["Responder_id"]
+ages = data["Age"]
+
+bins = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+
+# When result of some bars is too high, the graph can look
+# a bit weird, so in that case we can change the value to
+# logorithmic value using log
+plt.hist(ages, bins=bins, edgecolor="black", log=True)
 
 
-# displaying cumulative result
-plt.hist(ages, bins=20, cumulative=True)
+median_age = 28
+color = "#fc4f30"
 
+plt.axvline(median_age, color=color, label="Median Age")
+
+plt.legend()
+
+plt.title("Ages of responders")
+plt.xlabel("Ages")
+plt.ylabel("Responders")
+
+plt.tight_layout()
 plt.show()

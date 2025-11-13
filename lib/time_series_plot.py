642d763b8e207269cd6347fc13c7b53f553dedba
kaxp <kapilsahu232@gmail.com>
2025-11-13 17:07:51 +0530

    Add time series plot

---
+++ lib/time_series_plot.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib.dates as mpl_dates
+
+plt.style.use("seaborn-v0_8")
+
+data = pd.read_csv("lib/data/bitcoin_price.csv")
+
+# when dates are in string and not in DateTime we have to
+# convert them to DateTime using pandas
+data["Date"] = pd.to_datetime(data["Date"])
+data.sort_values("Date", inplace=True)
+
+price_date = data["Date"]
+price_close = data["Close"]
+
+plt.plot_date(price_date, price_close, linestyle="solid")
+
+# get current figure and auto format on x axis dates
+plt.gcf().autofmt_xdate()
+
+plt.title("Bitcoin Price")
+plt.xlabel("Date")
+plt.ylabel("Closing Price")
+
+# Formatting the date like May, 19 2024
+date_format = mpl_dates.DateFormatter("%b, %d %Y")
+plt.gca().xaxis.set_major_formatter(date_format)
+
+plt.tight_layout()
+plt.show()
